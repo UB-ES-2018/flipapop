da6f7176f206fa351e00b0acce1a8452084e2af5
jmcano94 <jmcano94@gmail.com>
2018-10-17 19:17:58 +0200

    Sub US 07 Backend: 	-Añadido bundle para trabajar con elastic search 	-Se ha configurado un finder de elastic search para buscar productos

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    FOS\ElasticaBundle\FOSElasticaBundle::class => ['all' => true],
 ];
