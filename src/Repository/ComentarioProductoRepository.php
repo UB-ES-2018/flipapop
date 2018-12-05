77dfb0dc34a595c185ee48c7fc9df221ca2d2590
jmcano94 <jmcano94@gmail.com>
2018-12-05 16:49:59 +0100

    Eliminadas funciones innecesarias en los repositorios

--- src/Repository/ComentarioProductoRepository.php
+++ src/Repository/ComentarioProductoRepository.php
@@ -19,32 +19,4 @@ class ComentarioProductoRepository extends ServiceEntityRepository
         parent::__construct($registry, ComentarioProducto::class);
     }
 
-    // /**
-    //  * @return ComentarioProducto[] Returns an array of ComentarioProducto objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('c')
-            ->andWhere('c.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('c.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?ComentarioProducto
-    {
-        return $this->createQueryBuilder('c')
-            ->andWhere('c.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
