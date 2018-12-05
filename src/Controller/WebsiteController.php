70c897ad267a71ec93d4f4de204d096097748843
Cristina <crreinal19@alumnes.ub.edu>
2018-12-05 16:36:27 +0100

    Añadido test para el sitemap

--- src/Controller/WebsiteController.php
+++ src/Controller/WebsiteController.php
@@ -10,10 +10,10 @@ namespace App\Controller;
 
 
 use App\Entity\Product;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class WebsiteController extends AbstractController
 {
